045a26fbda43a16f5f386c8a1d97a179fae213f8
Gonzalo Fleitas Pastor <gonzalo.fp340@gmail.com>
2015-12-28 20:50:39 -0300

    Primer commit

---
+++ example.py
@@ -0,0 +1,15 @@
+from TableConsole import Table
+
+a = Table()
+a.addColumn("ID", 5)
+a.addColumn("ARTICULO", 30)
+a.addColumn("MONEDA", 6)
+a.addColumn("PRECIO", 10)
+
+# Primer fila
+a.addRow(["1256", "Televisor LED", "USD", "800"])
+# Segunda fila
+a.addRow(["1256", "Heladera", "USD", "950"])
+# Tercera fila
+a.addRow(["1256", "Computadora", "USD", "450"])
+a.make()
